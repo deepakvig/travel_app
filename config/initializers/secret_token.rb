467b531ca78a0301c8b77f1aa6053e74b4ca24ae
Deepak Vig <deepakvig@gmail.com>
2013-01-17 16:41:13 +0530

    SAmple app skeleton added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravelApp::Application.config.secret_token = '7bc8e45cd19c2a6ee49317f3f9270363029db696693ab8a17b2b647b17c2d1f08dd995bb10081d3e86850643644dee92bbefdf771c761f11b9de715c4040d4ee'
